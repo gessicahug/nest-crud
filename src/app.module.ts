a880e9ef622e661ad9a4c5d31ceecef8c1a9c027
gessica da silva <gessicahugendobler@gmail.com>
2022-07-16 22:04:27 -0300

    controller, interface, dto, module and service created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [CatsModule],
 })
 export class AppModule {}
